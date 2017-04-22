24b01136a9be030c21033862bce5f47e1c9778f6
Thijs Koerselman <thijskoerselman@gmail.com>
2017-04-22 23:22:59 +0200

    Remove more unused code

--- src/reporters/index.js
+++
@@ -1,5 +0,0 @@
-/* @flow */
-
-export {default as ConsoleReporter} from './console/console-reporter.js';
-export {default as NoopReporter} from './noop-reporter.js';
-export {default as Reporter} from './base-reporter.js';
